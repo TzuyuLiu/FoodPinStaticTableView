ddd7d45d5a7a7df0d734d9b65376b8efb53a5e0a
TzuyuLiu <a2647803@yahoo.com.tw>
2019-05-05 23:13:12 +0800

    將新增頁面的輸入欄位變圓，並且將其內縮

--- FoodPin/View/RoundedTextField.swift
+++ FoodPin/View/RoundedTextField.swift
@@ -13,28 +13,32 @@ class RoundedTextField: UITextField {
     
     let padding = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
     
-    //以下三個override都是對文字縮排
+    //以下三個override都是對文字縮排，我們希望縮排文字到文字欄位中，因此override以下三個方法，回傳一個有縮排的矩形，使用UIEdgeInsets為文字欄為左右側增加邊距區域，呼叫bounds.inset(by: padding)來讓邊緣內縮排，已縮減矩形
     
-    //
+    //此方法所回傳的繪製矩陣是針對text區塊，重寫來重置文字區域
     override func textRect(forBounds bounds: CGRect) -> CGRect{
         
         return bounds.inset(by: padding)
     }
     
+    //此方法所回傳的繪製矩陣是針對place holder，重寫來重置place holder
     override func placeholderRect(forBounds bounds: CGRect) -> CGRect {
         
         return bounds.inset(by: padding)
     }
     
+    //此方法回傳的舉行是用於顯示可編輯的文字，重寫來重置輸入文字區域
     override func editingRect(forBounds bounds: CGRect) -> CGRect {
         
         return bounds.inset(by: padding)
     }
     
-    //建立圓角
+    //每次文字欄位佈局時都會呼叫layoutSubviews
     override func layoutSubviews() {
         super.layoutSubviews()
         
+        
+        //建立圓角
         self.layer.cornerRadius = 5.0
         
         self.layer.masksToBounds = true
